f4c78060bdcbe691ae31bb7aa150176bf0935fea
Fernando Soto <eldersoto1876@hotmail.com>
2022-05-28 01:40:58 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+const fullImgBox = document.getElementById("fullImgBox")
+fulImg = document.getElementById("fulImg")
+
+function openFullImg(reference) {
+    fullImgBox.style.display = "flex"
+    fulImg.src = reference
+}
+
+function closeImg() {
+    fullImgBox.style.display = "none";
+}
